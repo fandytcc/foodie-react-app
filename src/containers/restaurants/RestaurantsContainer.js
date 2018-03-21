b93afee86a1cd523c9de25d7d4d54807efb08db4
fandytcc <fandytsui@gmail.com>
2018-03-21 18:09:46 +0100

    add search fields for meals, location, price, dietary types

--- src/containers/restaurants/RestaurantsContainer.js
+++ src/containers/restaurants/RestaurantsContainer.js
@@ -2,14 +2,15 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 // import { push } from 'react-router-redux'
-import RestaurantItem from './RestaurantItem'
-import MapContainer from '../MapContainer'
-// import RestaurantEditor from './RestaurantEditor'
 import { restaurantShape } from './RestaurantPage'
 import { fetchRestaurants } from '../../actions/restaurants/fetch'
-
+import RestaurantItem from './RestaurantItem'
+// import RestaurantEditor from './RestaurantEditor'
+import MapContainer from '../MapContainer'
+import CheckboxList from '../../components/CheckboxList'
 //material-ui & styling
 import Typography from 'material-ui/Typography'
+import Divider from 'material-ui/Divider'
 import './RestaurantsContainer.css'
 
 class RestaurantsContainer extends PureComponent {
@@ -26,14 +27,43 @@ class RestaurantsContainer extends PureComponent {
   }
 
   render() {
-    if (!this.props.restaurants) return null
     const { restaurants } = this.props
-    console.log(this.props)
+    if (!restaurants) return null
+
+    const typesArray = Object.keys(restaurants[0].types)
+    const locationArray = restaurants.map(restaurant => restaurant.location.district)
+    const priceArray = Object.keys(restaurants[0].price)
+    const dietaryTypesArray = Object.keys(restaurants[0].dietaryType)
 
     return (
       <div className="main-container">
         <div className="side-bar">
           <MapContainer markers={restaurants} />
+
+          <div className="search-field">
+            <Typography variant="display1">Your Selections</Typography>
+            <Divider style={{ width: `150%` }}/>
+
+            <div className="types">
+              <Typography variant="display1">Meals</Typography>
+              <CheckboxList array={typesArray}/>
+            </div>
+
+            <div className="location">
+              <Typography variant="display1" component="h1">Location</Typography>
+              <CheckboxList array={locationArray}/>
+            </div>
+
+            <div className="price">
+              <Typography variant="display1" component="h1">Price</Typography>
+              <CheckboxList array={priceArray}/>
+            </div>
+
+            <div className="dietary-type">
+              <Typography variant="display1" component="h1">Dietary Types</Typography>
+              <CheckboxList array={dietaryTypesArray}/>
+            </div>
+          </div>
         </div>
 
         <div className="restaurants-container">
